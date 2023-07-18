e68f37335d55f8da36156cf150a7bd554ce7a545
David Tacoronte <davidtacoronte@Davids-MBP.attlocal.net>
2023-07-18 06:11:36 -0400

    Continued updating Redux structure - createStore function is in place

---
+++ Desktop/Personal Project folder/reactreduxproj1/src/redux/reducers/productReducer.js
@@ -0,0 +1,22 @@
+import { ActionTypes } from "../constants/action-types";
+
+const initialState = {
+    products:[
+        {
+        id:1,
+        title: "David",
+        category: "Software Developer",
+
+        },
+    ],
+
+};
+
+export const productReducer = (state, { type, payload }) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+        default:
+            return state
+    };
+};
