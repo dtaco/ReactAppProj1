bd9dfd077f8b8ecb0474e4309bc787f8992ca470
David Tacoronte <davidtacoronte@Davids-MBP.attlocal.net>
2023-07-19 17:13:12 -0400

    functionality was met - a succesful display of a working shop with state orgnaized and managed by Redux. Next is to add a cart functionality.

--- Desktop/Personal Project folder/reactreduxproj1/src/redux/reducers/productReducer.js
+++ Desktop/Personal Project folder/reactreduxproj1/src/redux/reducers/productReducer.js
@@ -1,22 +1,26 @@
 import { ActionTypes } from "../constants/action-types";
 
 const initialState = {
-    products:[
-        {
-        id:1,
-        title: "David",
-        category: "Software Developer",
-
-        },
-    ],
+    products: [],
 
 };
 
 export const productReducer = (state = initialState, { type, payload }) => {
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
-            return state;
+            return { ...state, products: payload };
         default:
-            return state
+            return state;
     };
 };
+
+export const selectedProductReducer = (state={}, {type, payload}) => {
+    switch (type) {
+        case ActionTypes.SELECTED_PRODUCT:
+            return {...state, ...payload}
+        case ActionTypes.REMOVE_SELECTED_PRODUCT:
+            return {};
+        default:
+                return state;
+    }
+}
