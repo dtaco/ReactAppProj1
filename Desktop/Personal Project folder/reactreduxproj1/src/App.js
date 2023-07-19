3058b3b9b86aa98de274bf761613f95e2c9d8888
David Tacoronte <davidtacoronte@Davids-MBP.attlocal.net>
2023-07-18 21:52:34 -0400

    updated project, now displaying

--- Desktop/Personal Project folder/reactreduxproj1/src/App.js
+++ Desktop/Personal Project folder/reactreduxproj1/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router , Routes , Route} from 'react-router-dom';
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+        <Route path="/" element={<ProductListing />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
